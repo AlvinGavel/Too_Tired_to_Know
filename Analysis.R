90a30cca979beabf28882678fc0f9a12bf50fe87
Alvin-Gavel <alvin.gavel@gmail.com>
2024-02-11 06:43:29 +0100

    Updated analysis to base self-rated performance on actual performance

--- Analysis.R
+++ Analysis.R
@@ -3,15 +3,35 @@ library(dplyr)
 
 # Loading and pre-processing of data
 kss = read.csv(file = 'Data/kss_data.csv')
-arit = kss[kss$test_type == 'M',]
-arit$is_sd = as.integer(arit$sd == 'Sleep Deprivation')
+
+arit = read.csv(file = 'Data/arithmetic_data.csv')
+arit_processed = arit %>% group_by(ID, order_of_test) %>% summarize(performance = mean(correct))
+
+arit_merged <- merge(arit_processed,
+                     kss[kss$test_type == 'M',],
+                     by.x=c("ID", "order_of_test"),
+                     by.y=c("id", "order_t"),
+                     all.x=FALSE,
+                     all.y=FALSE)
 
 # Here we will do the fitting of the cumulative model, following Bürkner and Vuorre
 
-fit_ad <- brm(
-  formula = rating3 ~ 1 + is_sd,
-  data = arit,
+arit_control <- brm(
+  formula = rating3 ~ 1 + performance,
+  data = arit_merged[arit_merged$sd == 'Control',],
   family = cumulative("probit")
 )
 
-summary(fit_ad)
+arit_test <- brm(
+  formula = rating3 ~ 1 + performance,
+  data = arit_merged[arit_merged$sd == 'Sleep Deprivation',],
+  family = cumulative("probit")
+)
+
+summary(arit_control)
+summary(arit_test)
+
+
+# TODO:
+# Main analysis defines performance as proportion correct, but make an additional
+# analysis (to go in an appendix) looking at answers per unit time
