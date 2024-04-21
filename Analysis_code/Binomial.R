30631f76ea5cd2bdc25e2055bca16fd4e13ae0fb
Alvin-Gavel <alvin.gavel@gmail.com>
2024-04-21 07:47:59 +0200

    Refactoring

--- Analysis_code/Binomial.R
+++ Analysis_code/Binomial.R
@@ -21,8 +21,8 @@ delta_steps <- 2 * n_steps - 1
 delta <- linspace(-1., 1., delta_steps)
 
 # Parameters in plotting
-control_colour <- "#0000FF"
-test_colour <- "#FF0033"
+colours <- c('test' = "#FF0033",
+             'control' = "#0000FF")
 
 performance_bounds <- list('arithmetic' = c(0.0,1.0),
                            'episodic memory' = c(0.0,1.0),
@@ -34,43 +34,26 @@ rating_bounds <- c(0,10)
 sleepiness_bounds <- c(0, 9)
 scatterplot_scatter <- 0.005
 
-
 P_insignificant <- c()
 for (i in 1:length(datasets)) {
   print(paste0('For ', dataset, ':'))
   
-  dataset = datasets[i]
-  n_correct <- 0
-  n_below_cont <- 0
-  n_above_test <- 0
-  n_below_test <- 0
+  dataset <- datasets[i]
+  n_acc <- c('test' = 0, 'control' = 0)
+  n_inacc <- c('test' = 0, 'control' = 0)
   for (time in 1:3) {
-    print(paste0('   For time ', time))
-    print(paste0('      The median self-rated performance in the control group was ', median_rating_cont[[dataset]][[time]]))
-    print(paste0('      The median self-rated performance in the test group was ', median_rating_test[[dataset]][[time]]))
-    print(paste0('      The median actual performance in the control group was ', format(median_performance_cont[[dataset]][[time]]), nsmall = 2))
-    print(paste0('      The median actual performance in the test group was ', format(median_performance_test[[dataset]][[time]]), nsmall = 2))
-    print(paste0('      The median reported sleepiness in the control group was ', median_sleepiness_cont[[dataset]][[time]]))
-    print(paste0('      The median reported sleepiness in the test group was ', median_sleepiness_test[[dataset]][[time]]))
-    rating_above_median_cont <- data_cont[[dataset]][[time]]$rating3 >= median_rating_cont[[dataset]][[time]]
-    rating_below_median_cont <- data_cont[[dataset]][[time]]$rating3 < median_rating_cont[[dataset]][[time]]
-    performance_above_median_cont <- data_cont[[dataset]][[time]]$performance >= median_performance_cont[[dataset]][[time]]
-    performance_below_median_cont <- data_cont[[dataset]][[time]]$performance < median_performance_cont[[dataset]][[time]]
-    sleepiness_above_median_cont <- data_cont[[dataset]][[time]]$rating1 >= median_sleepiness_cont[[dataset]][[time]]
-    sleepiness_below_median_cont <- data_cont[[dataset]][[time]]$rating1 < median_sleepiness_cont[[dataset]][[time]]
-    
-    rating_above_median_test <- data_test[[dataset]][[time]]$rating3 >= median_rating_test[[dataset]][[time]]
-    rating_below_median_test <- data_test[[dataset]][[time]]$rating3 < median_rating_test[[dataset]][[time]]
-    performance_above_median_test <- data_test[[dataset]][[time]]$performance >= median_performance_test[[dataset]][[time]]
-    performance_below_median_test <- data_test[[dataset]][[time]]$performance < median_performance_test[[dataset]][[time]]
-    sleepiness_above_median_test <- data_test[[dataset]][[time]]$rating1 >= median_sleepiness_test[[dataset]][[time]]
-    sleepiness_below_median_test <- data_test[[dataset]][[time]]$rating1 < median_sleepiness_test[[dataset]][[time]]
-    
-    # How prone were people to rate themselves accurately w.r.t. the median
-    n_acc_cont <- n_correct + sum(rating_above_median_cont & performance_above_median_cont)[[1]] + sum(rating_below_median_cont & performance_below_median_cont)[[1]]
-    n_inacc_cont <- n_below_cont + sum(rating_below_median_cont & performance_above_median_cont)[[1]] + sum(rating_above_median_cont & performance_below_median_cont)[[1]]
-    n_acc_test <- n_above_test + sum(rating_above_median_test & performance_above_median_test)[[1]] + sum(rating_below_median_test & performance_below_median_test)[[1]]
-    n_inacc_test <- n_below_test + sum(rating_below_median_test & performance_above_median_test)[[1]] + sum(rating_above_median_test & performance_below_median_test)[[1]]
+    for (i in 1:2) {
+      group = groups[i]
+      rating_above_median <- data[[dataset]][[group]][[time]]$rating3 >= median_rating[[dataset]][[group]][[time]]
+      rating_below_median <- data[[dataset]][[group]][[time]]$rating3 < median_rating[[dataset]][[group]][[time]]
+      performance_above_median <- data[[dataset]][[group]][[time]]$performance >= median_performance[[dataset]][[group]][[time]]
+      performance_below_median <- data[[dataset]][[group]][[time]]$performance < median_performance[[dataset]][[group]][[time]]
+      sleepiness_above_median <- data[[dataset]][[group]][[time]]$rating1 >= median_sleepiness[[dataset]][[group]][[time]]
+      sleepiness_below_median <- data[[dataset]][[group]][[time]]$rating1 < median_sleepiness[[dataset]][[group]][[time]]
+      # How prone were people to rate themselves accurately w.r.t. the median
+      n_acc[[group]] <- n_acc[[group]] + sum(rating_above_median & performance_above_median)[[1]] + sum(rating_below_median & performance_below_median)[[1]]
+      n_inacc[[group]] <- n_inacc[[group]] + sum(rating_below_median & performance_above_median)[[1]] + sum(rating_above_median & performance_below_median)[[1]]
+    }
   }
   
   ylab = c('Self-rated performance', 'Sleepiness')
@@ -90,54 +73,42 @@ for (i in 1:length(datasets)) {
       y_min <- ybounds[[y]][1]
       y_range <- ybounds[[y]][2] - y_min
       
-      cont_performance <- data_cont[[dataset]][[time]]$performance
-      cont_performance <- cont_performance + rnorm(length(cont_performance),
-                                                   mean=0,
-                                                   sd=x_range * scatterplot_scatter)
-      
-      cont_rating <- data_cont[[dataset]][[time]][[ydata[y]]]
-      cont_rating <- cont_rating + rnorm(length(cont_rating),
-                                         mean=0,
-                                         sd=y_range * scatterplot_scatter)
-      
-      test_performance <- data_test[[dataset]][[time]]$performance
-      test_performance <- test_performance + rnorm(length(test_performance),
-                                                   mean=0,
-                                                   sd=x_range * scatterplot_scatter)
-      
-      test_rating <- data_test[[dataset]][[time]][[ydata[y]]]
-      test_rating <- test_rating + rnorm(length(test_rating),
-                                         mean=0,
-                                         sd=y_range * scatterplot_scatter)
-      
-      points(cont_performance,
-             cont_rating,
-             cex=0.1,
-             pch=1,
-             col=c(control_colour))
-      points(test_performance,
-             test_rating,
-             cex=0.1,
-             pch=1,
-             col=c(test_colour))
+      for (i in 1:2) {
+        group = groups[i]
+        performance <- data[[dataset]][[group]][[time]]$performance
+        performance <- performance + rnorm(length(performance),
+                                           mean=0,
+                                           sd=x_range * scatterplot_scatter)
+        
+        rating <- data[[dataset]][[group]][[time]][[ydata[y]]]
+        rating <- rating + rnorm(length(rating),
+                                 mean=0,
+                                 sd=y_range * scatterplot_scatter)
+        
+        points(performance,
+               rating,
+               cex=0.1,
+               pch=1,
+               col=c(colours[[group]]))
+      }
     }
     legend(performance_bounds[[dataset]][1],
            y_min + 0.1 * y_range,
            legend=c("Control", "Test"),
            cex=0.8,
            pch=1,
-           col=c(control_colour, test_colour))
-    dev.off()}
+           col=c(colours[['control']], colours[[group]]))
+    dev.off()
+  }
   
+  for (i in 1:2) {
+    group = groups[i]
+    frac_acc <- n_acc[[group]] / (n_acc[[group]] + n_inacc[[group]])
+    print(paste0('In the ', group, ' group ', format(round(frac_acc * 100, 2), nsmall = 2), '% rated themselves accurately'))
+  }
   
-  # What fraction does that constitute?
-  frac_acc_cont <- n_acc_cont / (n_acc_cont + n_inacc_cont)
-  frac_acc_test <- n_acc_test / (n_acc_test + n_inacc_test)
-  print(paste0('In the control group ', format(round(frac_acc_cont * 100, 2), nsmall = 2), '% rated themselves accurately'))
-  print(paste0('In the test group ', format(round(frac_acc_test * 100, 2), nsmall = 2), '% rated themselves accurately'))
-  
-  L_cont <- L(n_acc_cont, n_inacc_cont, P_vector)
-  L_test <- L(n_acc_test, n_inacc_test, P_vector)
+  L_cont <- L(n_acc[[group]], n_inacc[[group]], P_vector)
+  L_test <- L(n_acc[[group]], n_inacc[[group]], P_vector)
   max_L = max(c(max(L_cont), max(L_test)))
   
   png(filename=paste0("Plots/", dataset, "/Metacognitive_performance.png"))
@@ -155,20 +126,20 @@ for (i in 1:length(datasets)) {
         lty = "solid",
         xaxs="i",
         yaxs="i",
-        col=control_colour)
+        col=colours[['control']])
   lines(P_vector,
         L_test,
         type="l",
         lty = "solid",
         xaxs="i",
         yaxs="i",
-        col=test_colour)
+        col=colours[['test']])
   legend(0,
          max_L,
          legend=c("Control", "Test"),
          cex=0.8,
          pch=1,
-         col=c(control_colour, test_colour))
+         col=c(colours[['control']], colours[['test']]))
   dev.off()
   
   p_delta <- convolve(L_cont, L_test, type = 'open')
--- Analysis_code/Preprocessing.R
+++ Analysis_code/Preprocessing.R
@@ -24,14 +24,15 @@ performance_indicators <- c('arithmetic' = 'correct',
                             'stroop' = 'correct', # It is possible that we want to use reaction_time here
                             'simple attention' = 'reaction_time')
 
-data_cont <- c()
-data_test <- c()
-median_performance_cont <- c()
-median_performance_test <- c()
-median_rating_cont <- c()
-median_rating_test <- c()
-median_sleepiness_cont <- c()
-median_sleepiness_test <- c()
+
+groups <- c('test', 'control')
+group_sd <- c('test' = 'Sleep Deprivation',
+              'control' = 'Control')
+
+data <- list()
+median_performance <-  list()
+median_rating <-  list()
+median_sleepiness <-  list()
 
 dir.create('Plots', showWarnings = FALSE)
 for (i in 1:length(datasets)) {
@@ -40,36 +41,52 @@ for (i in 1:length(datasets)) {
   dir.create(file.path('Plots', dataset), showWarnings = FALSE)
   
   performance_indicator <- as.character(performance_indicators[[dataset]])
-
-  data <- read.csv(file = paste0('Data/', filenames[[dataset]]))
-  data_processed <- data %>% group_by(ID, time) %>% summarize(performance = mean(.data[[performance_indicator]]))
-
+  
+  file_data <- read.csv(file = paste0('Data/', filenames[[dataset]]))
+  data_processed <- file_data %>% group_by(ID, time) %>% summarize(performance = mean(.data[[performance_indicator]]))
+  
   data_merged <- merge(data_processed,
-                      kss[kss$test_type == 'M',],
-                      by.x=c("ID", "time"),
-                      by.y=c("id", "time"),
-                      all.x=FALSE,
-                      all.y=FALSE)
-
-  data_cont[[dataset]] <- list()
-  data_test[[dataset]] <- list()
-  median_performance_cont[[dataset]] <- list()
-  median_performance_test[[dataset]] <- list()
-  median_rating_cont[[dataset]] <- list()
-  median_rating_test[[dataset]] <- list()
-  median_sleepiness_cont[[dataset]] <- list()
-  median_sleepiness_test[[dataset]] <- list()
-  for (time in 1:3) {
-    data_cont[[dataset]][[time]] <- data_merged[data_merged$sd == 'Control' & data_merged$time == time,]
-    data_test[[dataset]][[time]] <- data_merged[data_merged$sd == 'Sleep Deprivation' & data_merged$time == time,]
-
-    median_performance_cont[[dataset]][[time]] <- median(data_cont[[dataset]][[time]]$performance)
-    median_performance_test[[dataset]][[time]] <- median(data_test[[dataset]][[time]]$performance)
-
-    median_rating_cont[[dataset]][[time]] <- median(data_cont[[dataset]][[time]]$rating3)
-    median_rating_test[[dataset]][[time]] <- median(data_test[[dataset]][[time]]$rating3)
-    
-    median_sleepiness_cont[[dataset]][[time]] <- median(data_cont[[dataset]][[time]]$rating1)
-    median_sleepiness_test[[dataset]][[time]] <- median(data_test[[dataset]][[time]]$rating1)
+                       kss[kss$test_type == 'M',],
+                       by.x=c("ID", "time"),
+                       by.y=c("id", "time"),
+                       all.x=FALSE,
+                       all.y=FALSE)
+  
+  data[[dataset]] <- list()
+  median_performance[[dataset]] <- list()
+  median_rating[[dataset]] <- list()
+  median_sleepiness[[dataset]] <- list()
+  for (i in 1:2) {
+    group = groups[i]
+    data[[dataset]][[group]] <- list()
+    median_performance[[dataset]][[group]] <- list()
+    median_rating[[dataset]][[group]] <- list()
+    median_sleepiness[[dataset]][[group]] <- list()
+    for (time in 1:3) {
+      data[[dataset]][[group]][[time]] <- data_merged[data_merged$sd == group_sd[group] & data_merged$time == time,]
+      
+      median_performance[[dataset]][[group]][[time]] <- median(data[[dataset]][[group]][[time]]$performance)
+      # Lower is better for reaction time
+      if (performance_indicator == 'reaction_time') {
+        median_performance[[dataset]][[group]][[time]] <- - median_performance[[dataset]][[group]][[time]]
+      }
+      
+      median_rating[[dataset]][[group]][[time]] <- median(data[[dataset]][[group]][[time]]$rating3)
+      median_sleepiness[[dataset]][[group]][[time]] <- median(data[[dataset]][[group]][[time]]$rating1)
+      
+    }
+  }
+  for (i in 1:2) {
+    group = groups[i]
+    print(paste0('For the ', group, ' group'))
+    for (time in 1:3) {
+      print(paste0('   For time ', time))
+      print(paste0('      The median self-rated performance in the control group was ', median_rating[[dataset]][['control']][[time]]))
+      print(paste0('      The median self-rated performance in the test group was ', median_rating[[dataset]][['test']][[time]]))
+      print(paste0('      The median actual performance in the control group was ', format(median_performance[[dataset]][['control']][[time]]), nsmall = 2))
+      print(paste0('      The median actual performance in the test group was ', format(median_performance[[dataset]][['test']][[time]]), nsmall = 2))
+      print(paste0('      The median reported sleepiness in the control group was ', median_sleepiness[[dataset]][['control']][[time]]))
+      print(paste0('      The median reported sleepiness in the test group was ', median_sleepiness[[dataset]][['test']][[time]]))
+    }
   }
 }
